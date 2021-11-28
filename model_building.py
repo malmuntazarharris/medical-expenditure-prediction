d21d60b4ee2ee537cebf21b88a169c1ac547deb0
Malcolm Almuntazar-Harris <almuntazar.harris.malcolm@gmail.com>
2021-11-28 15:47:57 -0500

    Add model_building.py

---
+++ model_building.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 28 15:38:17 2021
+
+@author: Malcolm
+"""
+
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import numpy as np 
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv('C:/Users/Malcolm/Documents/MedicalExpenditure/data/meps_data_2019_new_feats.csv')
