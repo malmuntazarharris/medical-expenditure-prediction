c9aa40077e1f505a68317e5488b0419ecf7ff298
Malcolm Almuntazar-Harris <almuntazar.harris.malcolm@gmail.com>
2021-12-03 13:03:16 -0500

    Add basic html form

--- app/app.py
+++ app/app.py
@@ -1,39 +1,31 @@
+"""
+Resources: 
+ML Webapp example: https://github.com/krishnaik06/Deployment-flask/blob/master/app.py
+"""
+
 from flask import Flask, render_template, request
 import numpy as np
 import pickle
 
 app = Flask(__name__)
-
-posts = [
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-    }
-]
+# model = pickle.load(open('C:/Users/Malcolm/Documents/MedicalExpenditure/xgboost/pkl_objects/xgboost_model.pickle', 'rb'))
 
 @app.route('/')
-@app.route('/home')
 def home():
-    return render_template('home.html', posts=posts)
+    return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')    
+# @app.route('/predict',methods=['POST'])
+# def predict():
+#     '''
+#     Renders results in html
+#     '''
+#     int_features = [int(x) for x in request.form.values()]
+#     final_features = [np.array(int_features)]
+#     prediction = model.predict(final_features)
 
-def load_models():
-    file_name = 'C:/Users/Malcolm/Documents/MedicalExpenditure/xgboost/pkl_objects/xgboost_model.pickle'
-    with open(file_name, 'rb') as pickled:
-       data = pickle.load(pickled)
-       model = data['model']
-    return model
+#     output = round(prediction[0], 2)
+
+#     return render_template('index.html', prediction_text='Predicted Medical Expenditure per year is $ {}'.format(output))
 
 if __name__ == '__main__':
    app.run(debug=True)
