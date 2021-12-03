51a6c6738243805347839cf1689434a16fc34225
Malcolm Almuntazar-Harris <almuntazar.harris.malcolm@gmail.com>
2021-12-03 11:01:55 -0500

    Add template from freefrontend

--- app/app.py
+++ app/app.py
@@ -4,9 +4,29 @@ import pickle
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
 @app.route('/')
+@app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')    
 
 def load_models():
     file_name = 'C:/Users/Malcolm/Documents/MedicalExpenditure/xgboost/pkl_objects/xgboost_model.pickle'
