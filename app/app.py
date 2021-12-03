5470033302cf30c8143e31f9a3f42e758ce43341
Malcolm Almuntazar-Harris <almuntazar.harris.malcolm@gmail.com>
2021-12-02 22:06:31 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,20 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Dec  2 10:57:42 2021
-
-@author: Malcolm
-"""
-
-from flask import Flask
+from flask import Flask, render_template, request
 import numpy as np
 import pickle
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    test_np_input = np.ones(54).reshape(1, -1)
-    model = pickle.load(open('C:/Users/Malcolm/Documents/MedicalExpenditure/xgboost/pkl_objects/xgboost_model.pickle', 'rb'))
-    preds = model.predict(test_np_input)
-    preds_as_str = str(preds)
-    return preds_as_str
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+def load_models():
+    file_name = 'C:/Users/Malcolm/Documents/MedicalExpenditure/xgboost/pkl_objects/xgboost_model.pickle'
+    with open(file_name, 'rb') as pickled:
+       data = pickle.load(pickled)
+       model = data['model']
+    return model
+
+if __name__ == '__main__':
+   app.run(debug=True)
